1888b06416ead56af9d6be78bc2312e9d911c850
pavelzag <zagalsky@gmail.com>
2018-01-11 13:57:01 +0200

    Some return string clean ups

--- GoogleOCR.py
+++ GoogleOCR.py
@@ -25,7 +25,8 @@ def detect_text(file_name):
 
 def values_builder(texts, value_type=None):
     if not value_type:
-        return texts[0].description
+        string_before = texts[0].description
+        return string_before.replace('\n', ' ').replace('CH:4', '').replace('U', 'V')
     if value_type == 'voltage':
         # voltage = texts[1].description + texts[2].description
         voltage = texts[2].description
