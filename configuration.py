198176bb5ead902236b8333691c4b800909b6f36
pavelzag <zagalsky@gmail.com>
2018-01-17 11:35:13 +0200

    Some fixes

--- configuration.py
+++ configuration.py
@@ -7,5 +7,9 @@ with open(config_path, 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
 
+def get_config(parameter_name):
+    return cfg['creds'][parameter_name]
+
+
 def get_owner():
     return cfg['owner']
